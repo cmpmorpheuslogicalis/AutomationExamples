9ef5c33842f39157c14d3b7d01a75a3cabe3253c
cmpmorpheuslogicalis <108018099+cmpmorpheuslogicalis@users.noreply.github.com>
2022-07-06 10:43:20 -0300

    Update main.tf

--- Terraform/resource_group/main.tf
+++ Terraform/resource_group/main.tf
@@ -6,20 +6,6 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 
-terraform {
-  backend "azurerm" {
-  }
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.46"
-    }
-  }
-
-  required_version = ">= 0.12"
-}
-
 # Create a Resource Group
 resource "azurerm_resource_group" "rg1" {
   name = var.rg_name
