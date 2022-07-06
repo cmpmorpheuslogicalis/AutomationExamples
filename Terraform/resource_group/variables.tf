43d15af8cc2986eb1ba50ca21548f12f1fcca197
cmpmorpheuslogicalis <108018099+cmpmorpheuslogicalis@users.noreply.github.com>
2022-07-06 10:58:13 -0300

    Update variables.tf

--- Terraform/resource_group/variables.tf
+++ Terraform/resource_group/variables.tf
@@ -3,12 +3,12 @@
 #variable "client_secret" {}
 #variable "tenant_id" {}
 
-variable "rg_name" {
-  default = "morpheus_terraform_teste"
-  description = "Nome do grupo de recursos a ser criado"
-}
+#variable "rg_name" {
+#  default = "morpheus_terraform_teste"
+#  description = "Nome do grupo de recursos a ser criado"
+#}
 
-variable "rg_location" {
-  default = "East US"
-  description = "Local onde o grupo de recursos ficará"
-}
+#variable "rg_location" {
+#  default = "East US"
+#  description = "Local onde o grupo de recursos ficará"
+#}
