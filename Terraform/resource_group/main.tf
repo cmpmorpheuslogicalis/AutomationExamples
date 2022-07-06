b475cd1d7b98d9e30481638850fe2d58378efef9
cmpmorpheuslogicalis <108018099+cmpmorpheuslogicalis@users.noreply.github.com>
2022-07-06 10:57:48 -0300

    Update main.tf

--- Terraform/resource_group/main.tf
+++ Terraform/resource_group/main.tf
@@ -3,6 +3,16 @@ variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
 
+variable "rg_name" {
+  default = "morpheus_terraform_teste"
+  description = "Nome do grupo de recursos a ser criado"
+}
+
+variable "rg_location" {
+  default = "East US"
+  description = "Local onde o grupo de recursos ficará"
+}
+
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
