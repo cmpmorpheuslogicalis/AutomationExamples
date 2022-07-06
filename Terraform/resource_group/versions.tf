a2f0c5b2b88c748350fbfbc423f8a8463a5f31a1
cmpmorpheuslogicalis <108018099+cmpmorpheuslogicalis@users.noreply.github.com>
2022-07-06 11:15:08 -0300

    Update versions.tf

--- Terraform/resource_group/versions.tf
+++ Terraform/resource_group/versions.tf
@@ -1,13 +1,13 @@
-terraform {
-  backend "azurerm" {
-  }
+#terraform {
+#  backend "azurerm" {
+#  }
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.46"
-    }
-  }
+#  required_providers {
+#    azurerm = {
+#      source  = "hashicorp/azurerm"
+#      version = "~> 2.46"
+#    }
+#  }
 
-  required_version = ">= 0.12"
-}
+#  required_version = ">= 0.12"
+#}
